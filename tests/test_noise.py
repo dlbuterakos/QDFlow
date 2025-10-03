09a649a752c61d2284d67bbb1674b5ebf9bf6dde
db <>
2025-10-03 17:36:05 -0400

    noise.py tests (87% coverage)

--- tests/test_noise.py
+++ tests/test_noise.py
@@ -122,65 +122,238 @@ def test_random_noise_params():
 # Test NoiseGenerator
 # -------------------
 
-# class TestNoiseGenerator:
+class TestNoiseGenerator:
 
-#     @staticmethod
-#     def test_white_noise():
+    @staticmethod
+    def test_white_noise():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.white_noise(data, 1)
+        assert noisy.shape == data.shape
+        assert not np.allclose(noisy, data)
 
-# def make_simple_map(shape=(10, 10)):
-#     return np.ones(shape)
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.white_noise(data, 0)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy, data)
 
-# def test_white_noise():
-#     ng = noise.NoiseGenerator(noise.NoiseParameters(white_noise_magnitude=0.1))
-#     data = make_simple_map()
-#     noisy = ng.white_noise(data, 0.1)
-#     assert noisy.shape == data.shape
-#     assert not np.allclose(noisy, data)
+        data = np.array(np.arange(30), dtype=np.float64)
+        noisy = ng.white_noise(data, 1)
+        assert noisy.shape == data.shape
+        assert not np.allclose(noisy, data)
 
-# def test_pink_noise():
-#     ng = noise.NoiseGenerator(noise.NoiseParameters(pink_noise_magnitude=0.1))
-#     data = make_simple_map()
-#     noisy = ng.pink_noise(data, 0.1)
-#     assert noisy.shape == data.shape
-#     assert not np.allclose(noisy, data)
+    @staticmethod
+    def test_pink_noise():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.pink_noise(data, 1, axis=None)
+        assert noisy.shape == data.shape
+        assert not np.allclose(noisy, data)
 
-# def test_telegraph_noise():
-#     ng = noise.NoiseGenerator(noise.NoiseParameters())
-#     data = make_simple_map()
-#     noisy = ng.telegraph_noise(data, 0.2, 0.1, 2, 2, axis=0)
-#     assert noisy.shape == data.shape
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.pink_noise(data, 1, axis=0)
+        assert noisy.shape == data.shape
+        assert not np.allclose(noisy, data)
 
-# def test_line_shift():
-#     ng = noise.NoiseGenerator(noise.NoiseParameters())
-#     data = make_simple_map()
-#     shifted = ng.line_shift(data, ave_pixels=2, axis=0, shift_positive=True)
-#     assert shifted.shape == data.shape
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.pink_noise(data, 0, axis=None)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy, data)
 
-# def test_sech_blur():
-#     ng = noise.NoiseGenerator(noise.NoiseParameters())
-#     data = make_simple_map()
-#     blurred = ng.sech_blur(data, blur_width=2, noise_axis=0)
-#     assert blurred.shape == data.shape
+        data = np.array(np.arange(30), dtype=np.float64)
+        noisy = ng.pink_noise(data, 1, axis=None)
+        assert noisy.shape == data.shape
+        assert not np.allclose(noisy, data)
 
-# def test_sensor_gate():
-#     ng = noise.NoiseGenerator(noise.NoiseParameters())
-#     data = make_simple_map()
-#     sgc = np.array([0.1, 0.2])
-#     noisy = ng.sensor_gate(data, sensor_gate_coupling=sgc)
-#     assert noisy.shape == data.shape
+    @staticmethod
+    def test_telegraph_noise():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.telegraph_noise(data, 7., 0, 5, 5, axis=0)
+        assert noisy.shape == data.shape
+        assert np.allclose(np.abs(noisy - data), 3.5)
 
-# def test_unint_dot_add():
-#     ng = noise.NoiseGenerator(noise.NoiseParameters())
-#     data = make_simple_map()
-#     spacing = np.array([1.0, 0.0])
-#     noisy = ng.unint_dot_add(data, magnitude=0.1, spacing=spacing, width=1.0, offset=0.5)
-#     assert noisy.shape == data.shape
+        data = np.array(np.arange(30), dtype=np.float64)
+        noisy = ng.telegraph_noise(data, 7., 0, 5, 5, axis=0)
+        assert noisy.shape == data.shape
+        assert np.allclose(np.abs(noisy - data), 3.5)
 
-# def test_high_coupling_coulomb_peak():
-#     ng = noise.NoiseGenerator(noise.NoiseParameters())
-#     data = make_simple_map()
-#     out = ng.high_coupling_coulomb_peak(data, peak_offset=0.5, peak_width=1.0, peak_spacing=2.0)
-#     assert out.shape == data.shape
+    @staticmethod
+    def test_line_shift():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.line_shift(data, 1, axis=1, shift_positive=True)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy, np.round(noisy))
+        assert np.all(noisy < 30)
+        assert np.all(noisy[1:,:] > -1)
+
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.line_shift(data, 1, axis=1, shift_positive=False)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy, np.round(noisy))
+        assert np.all(noisy > -1)
+        assert np.all(noisy[:-1,:] < 30)
+
+        data = np.array(np.arange(30), dtype=np.float64)
+        noisy = ng.line_shift(data, 1, axis=0, shift_positive=True)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy, data)
+
+    @staticmethod
+    def test_latching_noise():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        excited = np.array(np.arange(30,60).reshape((5,6)), dtype=np.float64)
+        dot_charges = np.concatenate([np.full((5,6,1), 1, dtype=np.int_), 
+                np.concatenate([np.full((2,6,1), 1, dtype=np.int_), np.full((3,6,1), 2, dtype=np.int_)], axis=0)
+        ], axis=2)
+        are_dots_combined = np.full((5,6,1), False, dtype=np.bool_)
+        noisy = ng.latching_noise(data, excited, dot_charges, are_dots_combined, 1, axis=0, shift_positive=True)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy[0:2,:], data[0:2,:])
+        assert np.all(np.isclose(noisy, data) | np.isclose(noisy, excited))
+
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        excited = np.array(np.arange(30,60).reshape((5,6)), dtype=np.float64)
+        dot_charges = np.concatenate([np.full((5,6,1), 1, dtype=np.int_), 
+                np.concatenate([np.full((2,6,1), 1, dtype=np.int_), np.full((3,6,1), 2, dtype=np.int_)], axis=0)
+        ], axis=2)
+        are_dots_combined = np.full((5,6,1), False, dtype=np.bool_)
+        noisy = ng.latching_noise(data, excited, dot_charges, are_dots_combined, 1, axis=0, shift_positive=False)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy[2:,:], data[2:,:])
+        assert np.all(np.isclose(noisy, data) | np.isclose(noisy, excited))
+
+        data = np.array(np.arange(30), dtype=np.float64)
+        excited = np.array(np.arange(30,60), dtype=np.float64)
+        dot_charges = np.concatenate([np.full((30,1), 1, dtype=np.int_), 
+                np.concatenate([np.full((15,1), 1, dtype=np.int_), np.full((15,1), 2, dtype=np.int_)], axis=0)
+        ], axis=1)
+        are_dots_combined = np.full((30,1), False, dtype=np.bool_)
+        noisy = ng.latching_noise(data, excited, dot_charges, are_dots_combined, 1, axis=0, shift_positive=True)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy, data)
+
+    @staticmethod
+    def test_unint_dot_add():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.unint_dot_add(data, 50, np.array([5, 5]), 1, .5)
+        assert noisy.shape == data.shape
+        assert (noisy[4,5] - noisy[0,0]) > 29+75 and (noisy[4,5] - noisy[0,0]) < 29+125
+
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.unint_dot_add(data, 50, np.array([7, 7, 7]), .1, .5,
+                                 gate_data_matrix=np.array([[1,0],[0,1],[1,1]]))
+        assert noisy.shape == data.shape
+        assert (noisy[4,5] - noisy[0,0]) > 29+75 and (noisy[4,5] - noisy[0,0]) < 29+125
+
+        data = np.array(np.arange(30), dtype=np.float64)
+        noisy = noisy = ng.unint_dot_add(data, 50, np.array([15]), 1, .5)
+        assert noisy.shape == data.shape
+        assert (noisy[29] - noisy[15]) > 14+75 and (noisy[29] - noisy[15]) < 14+125
+        assert (noisy[15] - noisy[0]) > 15+75 and (noisy[15] - noisy[0]) < 15+125
+
+    @staticmethod
+    def test_sech_blur():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.array([[20,20,20,20,20,20,20],[1,0,1,0,1,0,1],[20,20,20,20,20,20,20]], dtype=np.float64)
+        noisy = ng.sech_blur(data, 3, noise_axis=1)
+        assert noisy.shape == data.shape
+        assert noisy[1,3] > .25
+        assert noisy[1,3] < 1
+
+        data = np.array([[20,20,20,20,20,20,20],[1,0,1,0,1,0,1],[20,20,20,20,20,20,20]], dtype=np.float64)
+        noisy = ng.sech_blur(data, 3, noise_axis=0)
+        assert noisy.shape == data.shape
+        assert noisy[1,3] > 1
+        assert noisy[1,3] < 20
+
+        data = np.array(np.arange(30), dtype=np.float64)
+        noisy = ng.sech_blur(data, 3, noise_axis=0)
+        assert noisy.shape == data.shape
+        assert np.isclose(noisy[15], data[15])
+        assert not np.isclose(noisy[2], data[2])
+        assert not np.isclose(noisy[28], data[28])
+
+        error = False
+        data = np.array([[20,20,20,20,20,20,20],[1,0,1,0,1,0,1],[20,20,20,20,20,20,20]], dtype=np.float64)
+        try:
+            noisy = ng.sech_blur(data, 3, noise_axis=4)
+        except ValueError:
+            error = True
+        assert error
+
+    @staticmethod
+    def test_sensor_gate():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.ones((5,6))
+        noisy = ng.sensor_gate(data, np.array([1.7, 3]), 1)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy, 1+np.add.outer(1.7*np.arange(5), 3*np.arange(6)))
+
+        data = np.ones((5,6))
+        noisy = ng.sensor_gate(data, np.array([1.7, 3, -.5]), 1, gate_data_matrix=np.array([[1,0],[0,1],[1,1]]))
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy, 1+np.add.outer(1.2*np.arange(5), 2.5*np.arange(6)))
+
+        data = np.ones(30)
+        noisy = noisy = ng.sensor_gate(data, np.array([1.7]), 1)
+        assert noisy.shape == data.shape
+        assert np.allclose(noisy, 1+1.7*np.arange(30))
+
+    @staticmethod
+    def test_coulomb_peak():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.coulomb_peak(data, 15, 5)
+        assert noisy.shape == data.shape
+        assert np.all((noisy >= 0) & (noisy <= 1))
+
+        data = np.array(np.arange(30), dtype=np.float64)
+        noisy = ng.coulomb_peak(data, 14.5, 5)
+        noisy2 = ng.coulomb_peak(data, 14.5, 10)
+        assert noisy.shape == data.shape
+        assert np.all((noisy >= 0) & (noisy <= 1))
+        assert np.allclose(noisy, np.flip(noisy))
+        assert noisy2.shape == data.shape
+        assert np.all(noisy2 > noisy)
+
+    @staticmethod
+    def test_high_coupling_coulomb_peak():
+        noise.set_rng_seed(456)
+        ng = noise.NoiseGenerator(noise.NoiseParameters())
+        data = np.array(np.arange(30).reshape((5,6)), dtype=np.float64)
+        noisy = ng.high_coupling_coulomb_peak(data, .5, 5, 10)
+        assert noisy.shape == data.shape
+        assert np.all((noisy >= 0))
+
+        data = np.array(np.arange(30), dtype=np.float64)
+        noisy = ng.high_coupling_coulomb_peak(data, .5, 3, 10)
+        noisy2 = ng.high_coupling_coulomb_peak(data, 0, 3, 10)
+        assert noisy.shape == data.shape
+        assert np.all((noisy >= 0))
+        assert noisy2.shape == data.shape
+        assert np.all((noisy2 >= 0))
+        assert np.all(noisy2[0:3] > noisy[0:3])
+        assert np.all(noisy2[3:8] < noisy[3:8])
+        assert np.all(noisy2[8:13] > noisy[8:13])
+        assert np.all(noisy2[13:18] < noisy[13:18])
+        assert np.all(noisy2[18:23] > noisy[18:23])
+        assert np.all(noisy2[23:28] < noisy[23:28])
+        assert np.all(noisy2[28:30] > noisy[28:30])
+
+
 
 # def test_calc_noisy_map_all_noise_types():
 #     params = noise.NoiseParameters(
